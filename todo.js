b2dc45f5d78497d281dd8f4eb0e80e827eca79fa
Sergij Pankiv <pankivsergij97@gmail.com>
2023-01-04 13:32:35 +0200

    Refactoring
    Code was refactored by replacing regular functions expressions on arrow functions syntax instead

--- todo.js
+++ todo.js
@@ -7,14 +7,14 @@ const filters = {
 
 renderTodos(todos, filters)
 
-document.querySelector('#search-text').addEventListener('input', function (e) {
+document.querySelector('#search-text').addEventListener('input', (e) => {
   filters.searchText = e.target.value
   renderTodos(todos, filters)
 })
 
-document.querySelector('#new-todo').addEventListener('submit', function (e) {
+document.querySelector('#new-todo').addEventListener('submit', (e) => {
   e.preventDefault()
-  const id = generateUID()
+  const id = uuidv4()
   todos.push({
     id: id,
     text: e.target.elements.text.value,
@@ -22,13 +22,11 @@ document.querySelector('#new-todo').addEventListener('submit', function (e) {
   })
     
   saveTodos(todos)
-  // renderTodos(todos, filters)
   location.assign(`/edit.html#${id}`)
-
   e.target.elements.text.value = ''
 })
 
-document.querySelector('#hide-completed').addEventListener('change', function (e) {
+document.querySelector('#hide-completed').addEventListener('change', (e) => {
   filters.hideCompleted = e.target.checked
   renderTodos(todos, filters)
 })
